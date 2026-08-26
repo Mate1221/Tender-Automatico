f47b835873b4a29987042eabc9a553485ad6bb6d
Mate1221 <matepinamar11@gmail.com>
2026-08-26 15:02:06 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+const estadoTender = document.querySelector(".estado_tender");
+const btnColor = document.querySelector(".btn_color");
+const broker = "wss://d95554d0e6434a35b9a224e50b0525ed.s1.eu.hivemq.cloud:8884/mqtt";
+
+const opciones = {
+    username: "TenderWeb",
+    password: "ETecnica1"
+};
+
+const cliente = mqtt.connect(broker, opciones);
+
+cliente.on("connect", () => {
+    console.log("Conectado a HiveMQ");
+    cliente.subscribe("tender/lluvia");
+});
+
+cliente.on("message", (topic, mensaje) => {
+    const estado = mensaje.toString();
+    console.log("Mensaje recibido:", estado);
+    estadoTender.textContent = estado;
+
+    btnColor.classList.remove(
+        "btn_color_rojo",
+        "btn_color_verde",
+        "btn_color_azul"
+    );
+
+    if (estado === "Mojado") {
+        btnColor.classList.add("btn_color_rojo");
+    } else if (estado === "Seco") {
+        btnColor.classList.add("btn_color_verde");
+    } else {
+        btnColor.classList.add("btn_color_azul");
+    }
+});
