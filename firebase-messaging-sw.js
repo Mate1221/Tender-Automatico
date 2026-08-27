cf96bba7b26b176af3c91aec5691f9adfb809e99
Mate1221 <matepinamar11@gmail.com>
2026-08-26 22:30:24 -0300

    Refactor Firebase initialization and messaging setup

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -6,60 +6,20 @@ importScripts(
     "https://www.gstatic.com/firebasejs/12.1.0/firebase-messaging-compat.js"
 );
 
-
 firebase.initializeApp({
-
     apiKey: "AIzaSyCxRb2S1VGQFv3RJl0epfIa4aNVYWnR73U",
-
-    authDomain:
-        "tender-automatico.firebaseapp.com",
-
-    projectId:
-        "tender-automatico",
-
-    storageBucket:
-        "tender-automatico.firebasestorage.app",
-
-    messagingSenderId:
-        "97815083485",
-
-    appId:
-        "1:97815083485:web:d9e052b9993917bd9b1e344"
-
+    authDomain: "tender-automatico.firebaseapp.com",
+    projectId: "tender-automatico",
+    storageBucket: "tender-automatico.firebasestorage.app",
+    messagingSenderId: "97815083485",
+    appId: "1:97815083485:web:d9e052b9993917bd9b1e344"
 });
 
-
-const messaging =
-    firebase.messaging();
-
+const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
-
     console.log(
         "[Firebase SW] Mensaje recibido:",
         payload
     );
-
-
-    const titulo =
-        payload.notification?.title ||
-        "Tender Automático";
-
-
-    const opciones = {
-
-        body:
-            payload.notification?.body ||
-            "Se detectó un cambio en el tender.",
-
-        icon: "/Tender-Automatico/icon.png"
-
-    };
-
-
-    self.registration.showNotification(
-        titulo,
-        opciones
-    );
-
 });
