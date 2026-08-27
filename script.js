3ff298d136c1aa2899f1dc51cdba51645f5b91d1
Mate1221 <matepinamar11@gmail.com>
2026-08-26 22:28:27 -0300

    Refactor Firebase messaging and MQTT integration

--- script.js
+++ script.js
@@ -33,26 +33,6 @@ const btnColor = document.querySelector(".btn_color");
 
 
 // ============================
-// SERVICE WORKER NORMAL
-// ============================
-
-if ("serviceWorker" in navigator) {
-
-    navigator.serviceWorker.register("sw.js")
-        .then(() => {
-            console.log("Service Worker registrado");
-        })
-        .catch((error) => {
-            console.error(
-                "Error registrando Service Worker:",
-                error
-            );
-        });
-
-}
-
-
-// ============================
 // NOTIFICACIONES FIREBASE
 // ============================
 
